11cede2357c82d99fd1e16d5aabe7a829428515d
3540206742 <164978194+3540206742@users.noreply.github.com>
2025-03-20 09:41:11 +0800

    Create dog.java

---
+++ ja/dog.java
@@ -0,0 +1,15 @@
+public class dog{
+  private:
+    String name;
+    int age;
+    String gender;
+  public:
+    dog(String na,int ag,String ge){
+      name=na;
+      age=ag;
+      gender=ge;
+    }
+    void hello(){
+      System.out.println("wang wang wang!");
+    }
+}
